d19dfd3171e7591559d826d390221f2f046d3bea
Vicky Kumar <vickykumar1592004@gmail.com>
2026-06-12 15:35:45 +0530

    fibonacci number implementation using memoization

---
+++ DSA/Recursion/fibonacciwithmemoization.java
@@ -0,0 +1,32 @@
+package Recursion;
+import java.util.Arrays;
+public class fibonacciwithmemoization {
+
+//    earlier for calculating fibonacci number for big value takes a lot of time because it computes a same
+//    value again and again means it was linear recurrance but using memoization technique we can compute large
+//    number in very efficeintly and fastly.
+//
+    public static void main(String[] args) {
+        int n = 100;
+
+        long[] dp = new long[n + 1];
+        Arrays.fill(dp, -1);
+
+        System.out.println(fibo(n, dp));
+    }
+
+    static long fibo(int n, long[] dp) {
+        if (n < 2) {
+            return n;
+        }
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+
+        dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp);
+
+        return dp[n];
+    }
+}
