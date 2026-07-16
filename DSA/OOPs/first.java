f5785ef3903582572751ad368ca6008bbedce538
Vicky Kumar <vickykumar1592004@gmail.com>
2026-07-16 22:38:12 +0530

    learning class

--- DSA/OOPs/first.java
+++ DSA/OOPs/first.java
@@ -1,4 +1,35 @@
 package OOPs;
 
+class Student {
+
+    String name;
+    int rollNo;
+    int age;
+
+    void study() {
+        System.out.println(name + " is studying");
+    }
+
+    void eat() {
+        System.out.println(name + " is eating");
+    }
+}
+
 public class first {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        s1.name = "Vicky";
+        s1.rollNo = 101;
+        s1.age = 21;
+
+        System.out.println(s1.name);
+        System.out.println(s1.rollNo);
+        System.out.println(s1.age);
+
+        s1.study();
+        s1.eat();
+    }
 }
